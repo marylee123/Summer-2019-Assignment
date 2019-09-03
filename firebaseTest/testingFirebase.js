457e00d696e6840eaebbc450461c9689d43f1b75
Mary Lee <mary.lee.ny@gmail.com>
2019-09-03 17:31:15 -0400

    figuring out firebase (and failing)

---
+++ firebaseTest/testingFirebase.js
@@ -0,0 +1,18 @@
+/*(function() {
+  const config = {
+    apiKey: "AIzaSyBSyFt6gS_xe_qszfTbYsbOljIUVSv7N0",
+    authDomain: "web-quickstart-5505a.firebaseapp.com",
+    databaseURL: "https://web-quickstart-5505a.firebaseio.com",
+    storageBucket: "web-quickstart-5505a.appspot.com",
+  };
+  firebase.initializeApp(config);
+}());*/
+const firebaseConfig = {
+  apiKey: "AIzaSyDvRSHGXF3S-S1O1N8SfPB7dpYev6xYXeM",
+  authDomain: "testing-a5693.firebaseapp.com",
+  databaseURL: "https://testing-a5693.firebaseio.com",
+  projectId: "testing-a5693",
+  storageBucket: "",
+  messagingSenderId: "684379660632",
+  appId: "1:684379660632:web:a1838d2685aecbf3"
+};
